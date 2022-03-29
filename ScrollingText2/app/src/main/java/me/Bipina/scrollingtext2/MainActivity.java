7553eebcb4cdcd265e666daabc627838539cf398
Bipina Poudel <bipina234@gmail.com>
2022-03-30 04:27:02 +0545

    Task2 of Scrolling test get initiated

---
+++ ScrollingText2/app/src/main/java/me/Bipina/scrollingtext2/MainActivity.java
@@ -0,0 +1,15 @@
+package me.Bipina.scrollingtext2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import me.mahakagg.scrollingtext2.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
